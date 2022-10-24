1bdfd3677e9c317c794a9bb605ded8865718e268
Paula Villagra <paula@gmail.com>
2022-10-23 22:43:43 -0300

    cosas arregladas

--- Prueba2/urls.py
+++ Prueba2/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from firstApp.views import index
+from firstApp.views import index,peluche
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/',index),
+    path('peluches/',peluche)
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request,'templatesApp/index.html')
 
 def peluche(request):
-    data={"imag1":"https://cdn.shopify.com/s/files/1/2675/4376/products/N-2109-485276_1_512x512.jpg?v=1632379297","imag2":"https://cdn.shopify.com/s/files/1/0770/2163/products/mmwash1.jpg?v=1651271060","imag3":"https://cdn.shopify.com/s/files/1/0416/8083/0620/products/610020-Zoom.1_600x.jpg?v=1657303214","prod1":"Kuromi 15cm","prod2":"My Melody 20cm","prod3":"Hello Kitty"}
+    data={"titulo":"Peluches","imag1":"https://cdn.shopify.com/s/files/1/2675/4376/products/N-2109-485276_1_512x512.jpg?v=1632379297","imag2":"https://cdn.shopify.com/s/files/1/0770/2163/products/mmwash1.jpg?v=1651271060","imag3":"https://cdn.shopify.com/s/files/1/0416/8083/0620/products/610020-Zoom.1_600x.jpg?v=1657303214","prod1":"Kuromi 15cm","prod2":"My Melody 20cm","prod3":"Hello Kitty"}
     return render(request,'templatesApp/productos.html',data)
